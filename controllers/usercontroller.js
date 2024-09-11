b08582831cfffe6a5c3401d1d427c4fbd8403738
deepakyadav184 <deepakyadav180804@gmail.com>
2024-09-11 14:33:53 +0530

    updation....

---
+++ controllers/usercontroller.js
@@ -0,0 +1,32 @@
+const users = require('../users.json')
+const fs = require('fs')
+
+function getALLUsers(req,res) {
+   res.json(users); 
+}
+
+function getParticularUsers(req,res) {
+    let id = parseInt(req.params.id)
+    let user = users.find((user)=>user.id === id);
+    res.json(user)
+}
+function addUser(req,res) {
+    console.log(req.body)
+    req.body.id = users.length + 1;
+    users.push(req.body)
+    fs.writeFile('users.json',JSON.stringify(users),(err)=>{
+        if (err) {
+            console.log("error")
+        }else{
+            res.end("added sucessfully")
+        }
+    })
+    
+    
+}
+
+module.exports = {
+    addUser,
+    getParticularUsers,
+    getALLUsers
+}
